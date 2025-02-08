97c2eebcda0891d3d05274f648df46993af6e3f7
Sandeep <sandeepdewangan7@gmail.com>
2025-02-08 16:37:12 +0530

    -

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -1,18 +1,28 @@
 import { NavLink } from "react-router-dom";
 
 const NavPage = () => {
-    return (
-        <div>
-            <ul>
-                <img src="logo.png" />
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/pricing">Pricing</NavLink></li>
-                <li><NavLink to="/product">Product</NavLink></li>
-                <li><NavLink to="/login">Login</NavLink></li>
-                <li><NavLink to="/worldwise">World Wise</NavLink></li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul style={{ listStyle: "none", display: "flex" }}>
+        <img src="logo.png" />
+        <li style={{ padding: "10px" }}>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li style={{ padding: "10px" }}>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li style={{ padding: "10px" }}>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li style={{ padding: "10px" }}>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li style={{ padding: "10px" }}>
+          <NavLink to="/worldwise">World Wise</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default NavPage;
