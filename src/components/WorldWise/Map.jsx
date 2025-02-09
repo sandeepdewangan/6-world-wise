efaad679cf62595f35b7a627872dc07fdec7427f
Sandeep <sandeepdewangan7@gmail.com>
2025-02-09 18:19:16 +0530

    Leaflet changing center of map (click event)

--- src/components/WorldWise/Map.jsx
+++ src/components/WorldWise/Map.jsx
@@ -1,14 +1,24 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "../../context/CityProvider";
+import { useEffect, useState } from "react";
 
 const Map = () => {
-  const [searchParam, setSearchParam] = useSearchParams();
+  const [searchParam] = useSearchParams();
+  const { cities } = useCities();
+  const [position, setPosition] = useState([40, 0]);
   // const navigate = useNavigate();
 
-  // const lat = searchParam.get("lat");
-  // const lng = searchParam.get("lng");
-  const position = [51.505, -0.09];
+  const lat = searchParam.get("lat");
+  const lng = searchParam.get("lng");
+
+  useEffect(
+    function () {
+      if (lat && lng) setPosition([lat, lng]);
+    },
+    [lat, lng]
+  );
 
   return (
     // <div onClick={() => navigate("form")}>
@@ -21,8 +31,8 @@ const Map = () => {
       }}
     >
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={position}
+        zoom={7}
         scrollWheelZoom={true}
         style={{ height: "500px" }}
       >
@@ -30,14 +40,24 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>{city.notes}</Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={position} />
       </MapContainer>
     </div>
   );
 };
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
 
 export default Map;
